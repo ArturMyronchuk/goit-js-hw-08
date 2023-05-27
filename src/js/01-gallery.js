f1758cac8ed5457f51446194775d4d002593fa55
Artur Myronchuk <rolax223@gmail.com>
2023-05-28 00:19:44 +0300

    fn

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import '.././css/01-gallery.css';
+
+const galleryItemsEl = document.querySelector('.gallery');
+const cardMarkup = createGalleryItem(galleryItems);
+galleryItemsEl.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryItem(galleryItems) {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`;
+    })
+    .join('');
+  return markup;
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const videoCurrentTime = 'videoplayer-current-time';
 
+const video = document.querySelector('#vimeo-player');
+const player = new Player(video, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(videoCurrentTime, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(videoCurrentTime)) || 0);
+
+// player
+//   .setColor('#d8e0ff')
+//   .then(function (color) {
+//     console.log('The new color value: #D8E0FF');
+//   })
+//   .catch(function (error) {
+//     console.log('An error occurred while setting the color');
+//   });
