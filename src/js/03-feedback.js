8e499b0811a1c835994dbce5f9405a56c16042c1
Artur Myronchuk <rolax223@gmail.com>
2023-05-29 23:19:27 +0300

    updd

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,9 @@
-import throttle from 'lodash.throttle';
+import throttle from '{lodash.throttle}';
 const LOCAL_KEY = 'feedback-form-state';
 
 form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', throttle(onInputData, 1000));
+form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
 
 let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
